f026b742f8d0df2f60bb956e50c1e8e9ce73c835
Emma Paris <satlok.vipra@gmail.com>
2019-12-06 16:20:30 +0530

    post

--- email1/posts/models.py
+++ email1/posts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django import forms
+from django.core import validators
+from django.core.validators import ValidationError
 # Create your models here.
 
 
 class Posts(models.Model):
-    title = models.CharField(max_length=255)
+    def min_length_check(val):
+        if len(val) <= 10:
+            raise  validators.ValidationError("Value must be greater than 10")
+
+    title = models.CharField(
+        validators=[
+            validators.validate_email,
+            min_length_check
+            ],
+        max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
