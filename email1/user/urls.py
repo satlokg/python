68d0cacd7a60c935ed80ebffb6f5003bdbb0a7df
Emma Paris <satlok.vipra@gmail.com>
2019-12-09 15:59:38 +0530

    auth

--- email1/user/urls.py
+++ email1/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from user.views import login, register
+from user.views import login, register, profile, logout
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
--- email1/user/views.py
+++ email1/user/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as authorize, logout as deauth
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html',{'title':'user login'})
+    if(request.method == "POST"):
+        form = AuthenticationForm()
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user is None:
+            messages.info(request, 'Username or Password is not correct')
+            return redirect('/user/login')
+        else:
+            authorize(request, user)
+            return redirect('/user/profile')
+    else:
+        if(request.user.is_authenticated):
+            return redirect('/user/profile')
+        form = AuthenticationForm()
+    return render(request, 'login.html',{'title':'user login','form':form})
 
 def register(request):
-    return render(request, 'register.html',{'title':'user register'})
+    form = UserCreationForm()
+    if(request.method == 'POST'):
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            messages.add_message(request, messages.INFO, 'User Successfully Registered')
+            return redirect('/user/login')
+    return render(request, 'register.html', {'title': 'user register', 'form': form})
+
+def logout(request):
+     if(request.user.is_authenticated):
+         deauth(request)
+         messages.info(request, 'You have been successfully logout')
+         return redirect('/user/login')
+
+def profile(request):
+    if(request.user.is_authenticated):
+        return render(request, 'profile.html')
+    else:
+        messages.info(request, 'You need to login first')
+        return redirect('/user/login')
+ 
